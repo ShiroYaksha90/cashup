77ff9dc126d0be9da5bcc63246ed9ac14e3951d8
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-07 12:57:33 +0300

    Generate migrations & add refs

---
+++ db/migrate/20230807095104_create_join_table_groups_entities.rb
@@ -0,0 +1,7 @@
+class CreateJoinTableGroupsEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :groups, :entities do |t|
+      t.index [:group_id, :entity_id]
+    end
+  end
+end
