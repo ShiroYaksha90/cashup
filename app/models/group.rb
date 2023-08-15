12b908025051f70979d2ca2b3fc832a2c387d903
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-15 20:24:08 +0300

    Update files

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
-    belongs_to :user
-    has_many :entities through :group_id
-    validates :name, presence true
-    validates :icon, presence true
+    belongs_to :user, through: :author_id
+    has_many :entities, through: :group_id
+    validates :name, presence: true
+    validates :icon, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  devise_scope :user do
-  root "devise/sessions#new"
-  end
+  root 'pages#home'
+  get 'pages/home', to: 'pages#home'
+  resources :users, only: [:index, :show]
 end
