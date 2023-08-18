6aceda3ee8d33d125ac1b2f6a0f5ecac793ddd21
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-18 03:47:03 +0300

    Add total_amount method to group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,14 @@ class Group < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :icon, presence: true
+
+  def self.total_amount(user)
+    total_amount = 0
+    @groups = Group.where(author_id: user) || []
+    @groups.each do |group|
+      amount = group.entities.sum(:amount)
+      total_amount += amount
+    end
+    total_amount
+  end
 end
