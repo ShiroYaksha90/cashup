0ad64b14b56adc4551a1786e763592150cfa687e
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-18 03:43:17 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'devise/registrations',
+    sessions: 'devise/sessions'
+  }
+ 
+   devise_scope :user do
+    authenticated do
+    root to: 'groups#index', as: 'user'
+    end
+    unauthenticated do
+      root to: 'pages#home', as: 'unauthenticated_root'
+    end
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
 
-  # Defines the root path route ("/")
-  root 'pages#home'
-  get 'pages/home', to: 'pages#home'
+  # root 'pages#home'
+  # get 'pages/home', to: 'pages#home'
   resources :users, only: [:index, :show] do
     resources :groups
     resources :entities
