7e2ba6c78603be687a08865f0cf94d0e880f0a6d
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-20 01:19:44 +0300

    Fixing bugs

--- spec/requests/entities_spec.rb
+++ spec/requests/entities_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
-
 RSpec.describe 'Entities', type: :request do
   include Devise::Test::IntegrationHelpers
-
   describe 'GET /new' do
     before do
-      @user = User.create(name: 'Basim', email: 'aljazaeribasim@gmail.com', password: '123123',
+      @user = User.create(name: 'Basim', email: 'test@gmail.com', password: '123123',
                           password_confirmation: '123123')
       sign_in @user
-      @group = Group.create!(name: 'category', icon: 'img', author_id: @user.id)
-      get new_user_entity_path(@user, group_id: @group)
+      @group = Group.create(name: 'category', icon: 'img', author_id: @user.id)
+      @user.save!
+      @group.save!
+      get new_user_entity_path(user_id: @user.id, group_id: @group.id)
     end
-
     it 'response to html' do
       expect(response.content_type).to include 'text/html'
     end
--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
-
 RSpec.describe 'Groups', type: :request do
   include Devise::Test::IntegrationHelpers
-
   describe 'GET /new' do
     before do
-      @user = User.create(name: 'Basim', email: 'aljazaeribasim@gmail.com', password: '123123',
+      @user = User.create(name: 'Basim', email: 'test@gmail.com', password: '123123',
                           password_confirmation: '123123')
       sign_in @user
       get user_path
+      get new_user_group_path(@user)
     end
-
     it 'response to html' do
       expect(response.content_type).to include 'text/html'
     end
